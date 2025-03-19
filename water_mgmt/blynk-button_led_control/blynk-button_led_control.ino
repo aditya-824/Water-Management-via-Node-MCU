43fc3610da4e03b4365358548e78c0b3a52562e9
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-03-19 11:21:42 +0530

    Test code for relay and resistive sensor

---
+++ water_mgmt/blynk-button_led_control/blynk-button_led_control.ino
@@ -0,0 +1,28 @@
+#define BLYNK_TEMPLATE_ID "TMPL36vXxI_07"
+#define BLYNK_TEMPLATE_NAME "Node MCU"
+#define BLYNK_AUTH_TOKEN "d0O7545ihpkl7vVlWAARTCuamgrc5-fb"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Mangalyaan";
+char pass[] = "leena321";
+
+BLYNK_WRITE(V0){
+  int value = param.asInt();
+  digitalWrite(LED_BUILTIN, value);
+  Serial.print("LED Value: ");
+  Serial.println(value);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Blynk.begin(auth, ssid, pass);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run();
+}
--- water_mgmt/water_mgmt.ino
+++ water_mgmt/water_mgmt.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#define MOISTURE_SENSOR_PIN A0
+#define RELAY_PIN 6
 
+#define DRY_VALUE 4095  // Adjust this value based on sensor's readings when dry
+#define WET_VALUE 5     // Adjust this value based on sensor's readings when fully wet
+
+void setup() {
+  pinMode(MOISTURE_SENSOR_PIN, INPUT);
+  // pinMode(RELAY_PIN, OUTPUT);
+  Serial.begin(9600);
+  delay (1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(MOISTURE_SENSOR_PIN);
 
+  // Map the sensor value to a percentage (0-100)
+  int percentage = map(sensorValue, DRY_VALUE, WET_VALUE, 0, 100);
+
+  // Ensure the percentage is within the valid range
+  percentage = constrain(percentage, 0, 100);
+
+  // Print the sensor value and percentage
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorValue);
+  Serial.print(", Moisture Level: ");
+  Serial.print(percentage);
+  Serial.println("%");
+
+//   // Relay Test
+//   digitalWrite(RELAY_PIN, HIGH);
+//   delay(2000);
+//   digitalWrite(RELAY_PIN, LOW);
+//   delay(2000);
 }
