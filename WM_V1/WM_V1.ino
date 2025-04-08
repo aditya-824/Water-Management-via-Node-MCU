f382801ab48d9ab1ca789162d995dabdee1e9d49
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-04-08 22:12:16 +0530

    Updated file nomenclature

--- WM_V1/WM_V1.ino
+++ WM_V1/WM_V1.ino
@@ -1,23 +1,17 @@
 /* ESP & Blynk includes */
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <BlynkTimer.h>
-
-/* Wi-Fi information for wireless connection */
-char auth[] = "YourBlynkAuthToken";
-char ssid[] = "YourWiFiSSID";
-char pass[] = "YourWiFiPassword";
 
 /* Pin defines */
 #define MOISTURE_SENSOR_PIN A0
 #define RELAY_PIN 6
-#define WATER_LEVEL_PIN 4
 /* Threshold defines */
 #define DRY_VALUE 4095  // Adjust this value based on sensor's readings when dry
 #define WET_VALUE 5     // Adjust this value based on sensor's readings when fully wet
 
-int moistureThreshold = 500;
-unsigned long pumpRunTime = 5000;
+int moistureThreshold = 500;  // Moisture threshold above which watering occurs
+int pumpRunTime = 2000;       // To control pump duration
+int soilMoisture = 0;         // To store the soil moisture value
+bool relayState = false;      // To store the relay state
 
 /* Function prototypes */
 void checkSoilMoisture();
@@ -26,65 +20,36 @@ void waterPlant();
 void setup() {
   pinMode(MOISTURE_SENSOR_PIN, INPUT);
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(WATER_LEVEL_PIN, INPUT);
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
-  timer.setInterval(60000L, checkSoilMoisture);
-  delay (1000);
+  delay(1000);
 }
 
 void loop() {
-  Blynk.run();
-  timer.run();
-
-  // int sensorValue = analogRead(MOISTURE_SENSOR_PIN);
-
-  // // Map the sensor value to a percentage (0-100)
-  // int percentage = map(sensorValue, DRY_VALUE, WET_VALUE, 0, 100);
-
-  // // Ensure the percentage is within the valid range
-  // percentage = constrain(percentage, 0, 100);
-
-  // // Print the sensor value and percentage
-  // Serial.print("Sensor Value: ");
-  // Serial.print(sensorValue);
-  // Serial.print(", Moisture Level: ");
-  // Serial.print(percentage);
-  // Serial.println("%");
-
-
-//   // Relay Test
-//   digitalWrite(RELAY_PIN, HIGH);
-//   delay(2000);
-//   digitalWrite(RELAY_PIN, LOW);
-//   delay(2000);
+  checkSoilMoisture();
+  waterPlant();
+  printData();
 }
 
 void checkSoilMoisture() {
-  int moistureValue = analogRead(SOIL_MOISTURE_PIN);
-  if (moistureValue < moistureThreshold) {
-    waterPlant();
-  }
+  soilMoisture = analogRead(MOISTURE_SENSOR_PIN);
 }
 
 void waterPlant() {
-  if (digitalRead(WATER_LEVEL_PIN) == HIGH) {
-    digitalWrite(RELAY_PIN, LOW);
-    delay(pumpRunTime);
+  if (soilMoisture >= moistureThreshold) {
     digitalWrite(RELAY_PIN, HIGH);
+    relayState = true;
+    delay(pumpRunTime);
+    digitalWrite(RELAY_PIN, LOW);
+    relayState = false;
+  } else {
+    digitalWrite(RELAY_PIN, LOW);
+    relayState = false;
   }
 }
 
-BLYNK_WRITE(V1) {
-  if (param.asInt() == 1) {
-    waterPlant();
-  }
-}
-
-BLYNK_WRITE(V2) {
-  timer.setInterval(param.asInt() * 1000L, checkSoilMoisture);
-}
-
-BLYNK_WRITE(V3) {
-  moistureThreshold = param.asInt();
+void printData() {
+  Serial.print("Moisture Level: ");
+  Serial.print(soilMoisture);
+  Serial.print(", Relay State: ");
+  Serial.println(relayState);
 }
